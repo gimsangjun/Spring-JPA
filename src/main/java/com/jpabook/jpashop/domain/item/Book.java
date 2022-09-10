2909d5b92b9ad068daa13ded5b398c302631a942
gimsangjun <5321913@naver.com>
2022-09-10 23:16:22 +0900

    entity 추가

---
+++ src/main/java/com/jpabook/jpashop/domain/item/Book.java
@@ -0,0 +1,15 @@
+package com.jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("B")
+@Setter @Getter
+public class Book extends Item {
+    private String author;
+    private String isbn;
+}
